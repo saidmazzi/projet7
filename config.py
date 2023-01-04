fb029d23a32c4a3c0b9b286f0a18f7157b2c2b50
saidmazzi <mazzisaid@gmail.com>
2023-01-04 16:27:51 +0100

    Modification

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-# To generate a new secret key:
-# >>> import random, string
-# >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
-FB_APP_ID = 1200420960103822
+SECRET_KEY = " "
+
+APP_ID = 
